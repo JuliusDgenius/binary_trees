e02707171e8c2fdfdf6559046df67f9efa12a951
JuliusDgenius <ibejulius1@gmail.com>
2022-08-29 19:55:19 +0100

    task

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -10,9 +10,9 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
 	binary_tree_t *left = NULL;
 	binary_tree_t *right = NULL;
-	binary_tree_t *parent = (parent != NULL ? node->parent : NULL);
+	binary_tree_t *parent = (node != NULL ? node->parent : NULL);
 
-	if ((parent != NULL) && (parent != NULL))
+	if ((parent != NULL) && (parent->parent != NULL))
 	{
 		left = parent->parent->left;
 		right = parent->parent->right;
