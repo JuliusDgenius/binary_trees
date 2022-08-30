7aa57a85bc575eaec5682d28e8ee29a2ada9532c
JuliusDgenius <ibejulius1@gmail.com>
2022-08-30 14:15:30 +0100

    task

--- 104-binary_tree_rotate_right.c
+++ 104-binary_tree_rotate_right.c
@@ -8,7 +8,7 @@
  */
 binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree)
 {
-	binary_tree_t *root, *new_root, *new_root_lc, *root_p;
+	binary_tree_t *root,  *new_root, *new_root_rc, *root_p;
 
 	if (tree == NULL)
 		return (NULL);
@@ -17,20 +17,22 @@ binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree)
 	{
 		if ((root->parent != NULL) && (root->parent->left == root))
 		{
-			if (root_p->parent != NULL && (root->parent->left == root))
-			{
-				if (root_p->parent != NULL && (root_pparent->left == root_p))
-					root_p->parent->left = root;
-				if (root_p->parent != NULL && (root_p->parent->right == root_p))
-					root_p->parent->right = root;
-				root->parent = root_p->parent;
-				root->left = NULL, root_p->parent = root;
-				root->right = root_p;
-			}
+			if (root_p->parent != NULL && (root_p->parent->left == root_p))
+				root_p->parent->left = root;
+			if (root_p->parent != NULL && (root_p->parent->right == root_p))
+				root_p->parent->right = root;
+			root->parent = root_p->parent;
+			root_p->left = NULL, root_p->parent = root;
+			root->right = root_p;
 		}
-		else
+	}
+	else
+	{
+		if (root->left != NULL)
 		{
-			if (root->left != NULL)
+			new_root = root->left, new_root->parent = root->parent;
+			new_root_rc = new_root->right, new_root->right = root;
+			if (root->parent != NULL)
 			{
 				if (root->parent->left == root)
 					root->parent->left = new_root;
